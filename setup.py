22ffd05ea2ffaa6551b41316cf658d117de47c66
Martin O'Reilly <developer@mtspace.net>
2018-09-10 18:00:14 +0100

    Modify setup.py to work around numpy dependency in ecos setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import subprocess
 from setuptools import setup
+
+# Install numpy separately as ecos dependency for cvxpy installation fails if numpy
+# is not already installed
+subprocess.Popen(["python", '-m', 'pip', 'install', 'numpy'])
 
 setup(name='SigNet',
       version='0.1.0',
@@ -7,5 +12,5 @@ setup(name='SigNet',
       author='Peter Davies, Aldo Glielmo',
       author_email='p.w.davies@warwick.ac.uk, aldo.glielmo@kcl.ac.uk',
       packages=['signet'],
-      install_requires=['numpy','scipy','scikit-learn','cvxpy', 'networkx'],
+      install_requires=['scikit-learn','cvxpy', 'networkx'],
       zip_safe=False)
